94d0cec665e27c4fd06344395005fca182090898
Jens de Rond <derond@redkiwi.nl>
2017-11-27 16:22:14 +0100

    Add shopping list model and remove dummy instances

--- model/recipe.model.js
+++ model/recipe.model.js
@@ -18,12 +18,6 @@ const RecipeSchema = new Schema({
 });
 
 
-const Recipe = mongoose.model('Recipe', RecipeSchema);
-
-// Add a 'dummy' Recipe (every time you require this file!)
-const Recipe = new Recipe({
-    name: 'Tomato soup',
-    description: 'Tomato soup is a soup made with tomatoes as the primary ingredient.',
-}).save();
+const Recipe = mongoose.model('recipe', RecipeSchema);
 
 module.exports = Recipe;
